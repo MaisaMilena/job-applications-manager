25926398f370e27c0931e1cf0a91c402c4622069
Maisa <maisa@getstic.ky>
2023-08-09 18:15:11 -0300

    Inital project

---
+++ public/script.js
@@ -0,0 +1,26 @@
+function populateTable(data) {
+    const dataContainer = document.getElementById("content-list");
+    data.forEach((item) => {
+        const div = document.createElement("div");
+        div.classList.add("application-card");
+        div.innerHTML = createCardContent(item)
+        dataContainer.appendChild(div);
+    });
+}
+
+function createCardContent(item) {
+    return `
+        <h1><a href="${item.website}" target="_blank"">${item.name}</a></h1>
+        <p><b>Role</b>: ${item.role}</p>
+        <p><b>Status</b>: ${item.status}</p>
+    ` 
+}
+
+fetch("/data")
+    .then(response => response.json())
+    .then(data => {
+        populateTable(data);
+    })
+    .catch(error => {
+        console.error("Error fetching JSON data:", error);
+    });
---
+++ server.js
@@ -0,0 +1,27 @@
+const express = require('express')
+const fs = require('fs'); 
+const app = express()
+const port = 3000
+
+app.use(express.static(__dirname + '/public'));
+
+app.listen(port, () => 
+    console.log('Servidor iniciado na porta 3000')
+);
+
+app.get('/', (req, res) => {
+    res.send(__dirname + 'index.html')
+});
+
+app.get('/data',(req, res) => {
+    fs.readFile(__dirname + '/data.json', 'utf8', (err, data) => {
+        if (err) {
+            console.error('Error reading data.json:', err);
+            res.status(500).send('Internal Server Error');
+            return;
+        }
+
+        const jsonData = JSON.parse(data);
+        res.send(jsonData);
+    });
+})
